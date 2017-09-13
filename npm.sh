d198adbe16214d54474d59a7cba119faf8934dda
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-09-13 09:06:34 -0400

    Add shell script for default npm packages

---
+++ npm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+npm i -g npm
+npm i -g standard
+npm i -g stylelint
+npm i -g heroku-cli
+(
+  export PKG=eslint-config-airbnb;
+  npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install -g "$PKG@latest"
+)
