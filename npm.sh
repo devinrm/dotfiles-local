17749633c5ff008570e9202d8df45eeb7cfa6f9c
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-10-14 17:28:50 -0400

    Update node version

--- npm.sh
+++ npm.sh
@@ -5,6 +5,10 @@ npm i -g standard
 npm i -g stylelint
 npm i -g heroku-cli
 npm i -g tern
+npm i -g prettier
+npm i -g eslint-plugin-prettier
+npm i -g eslint-config-prettier
+
 (
   export PKG=eslint-config-airbnb;
   npm info "$PKG@latest" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install -g "$PKG@latest"
