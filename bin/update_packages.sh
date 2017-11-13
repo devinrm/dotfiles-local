3e6312830d6245c08bbb995c11720c8ac7dbb13c
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-11-13 11:18:12 -0500

    Remove easymotion nN mappings cause they don't work with *

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -2,6 +2,6 @@
 
 brew update && brew upgrade ;
 gem update ;
-npm outdated -g --depth=0 && npm update -g ;
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U ;
-pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U
+pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U ;
+npm outdated -g --depth=0 && npm update -g
