124ce7a29c93221f3d1adfe9ee88bffe61b3483a
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-11-14 08:19:19 -0500

    New alias and cleanup brew after updating

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -4,4 +4,6 @@ brew update && brew upgrade ;
 gem update ;
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U ;
 pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U ;
-npm outdated -g --depth=0 && npm update -g
+npm outdated -g --depth=0 && npm update -g ;
+brew cleanup ;
+brew prune
