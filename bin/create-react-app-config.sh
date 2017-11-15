98c804e8b3b4a8aae5245cd0b243a4641199b9fe
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-11-15 11:45:33 -0500

    Update create-react-app-config

--- bin/create-react-app-config.sh
+++ bin/create-react-app-config.sh
@@ -13,4 +13,12 @@ yarn add --dev eslint-plugin-no-unsanitized ;
 yarn add --dev eslint-plugin-prettier ;
 yarn add --dev eslint-plugin-react ;
 yarn add --dev flow-bin ;
-yarn add --dev prettier
+yarn add --dev prettier ;
+
+yarn add --dev babel-core ;
+yarn add --dev babel-loader ;
+yarn add --dev babel-plugin-transform-runtime ;
+yarn add --dev babel-preset-env ;
+yarn add --dev babel-preset-react ;
+yarn add --dev babel-preset-stage-1 ;
+yarn add babel-runtime
