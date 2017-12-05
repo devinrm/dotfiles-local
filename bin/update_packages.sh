4a6db213487c8db506fcbd89b0e913a7edebc713
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-12-05 11:07:17 -0500

    Update packages script

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 brew update && brew upgrade ;
+brew cleanup ;
+brew prune ;
 gem update ;
+gem update --system ;
+gem pristine bundler ;
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U ;
 pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U ;
-npm outdated -g --depth=0 && npm update -g ;
-brew cleanup ;
-brew prune
+npm outdated -g --depth=0 && npm update -g
