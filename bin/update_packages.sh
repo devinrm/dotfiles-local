9c64ba06689284a097f0f4c3e4083f5ff918caba
Devin Morgenstern <devin.morgenstern@protonmail.com>
2018-01-30 09:29:53 -0500

    Update docsets with update script

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -12,4 +12,5 @@ pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -
 pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U ;
 asdf update ;
 asdf plugin-update --all ;
+dasht-docsets-update
 npm outdated -g --depth=0 && npm update -g
