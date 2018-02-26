5f434280e027f8c64ffe4259a605f57aee87ab97
Devin Morgenstern <devin.morgenstern@protonmail.com>
2018-02-25 19:20:39 -0500

    Minor change, commit message skipped

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -13,4 +13,5 @@ pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -
 asdf update ;
 asdf plugin-update --all ;
 dasht-docsets-update
-npm outdated -g --depth=0 && npm update -g
+# npm outdated -g --depth=0
+npm update -g
