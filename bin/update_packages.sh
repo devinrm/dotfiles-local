99c26cac4e34c7e4d1f09df41ee40560e6de89a3
Devin Morgenstern <devin.morgenstern@protonmail.com>
2018-04-02 20:39:10 -0400

    Fix asdf python

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -12,6 +12,8 @@ pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -
 pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U ;
 asdf update ;
 asdf plugin-update --all ;
+# the asdf python maintainer doesn't update pyenv regularly so this is necessary
+cd ~/.asdf/plugins/python/pyenv/ && git pull && cd -
 dasht-docsets-update
 npm outdated -g --depth=0
 npm update -g
