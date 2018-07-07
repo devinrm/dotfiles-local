33e47cc827929d9d9e828a6c059e76d6950bc7b5
Devin Morgenstern <devin.morgenstern@protonmail.com>
2018-07-07 11:48:35 -0400

    minor change, commit message skipped

--- bin/pip.sh
+++ bin/pip.sh
@@ -11,4 +11,5 @@ pip install solargraph-utils.py ;
 pip install pgcli ;
 pip install --upgrade pip ;
 pip2 install --upgrade pip ;
-pip3 install --upgrade pip
+pip3 install --upgrade pip ;
+pip install 'prompt_toolkit<1.1.0,>=1.0.10'
