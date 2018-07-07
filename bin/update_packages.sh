9127e0e68767dbfe3a473260eff96ef7222064d1
Devin Morgenstern <devin.morgenstern@protonmail.com>
2018-07-07 12:28:08 -0400

    minor change, commit message skipped

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -8,6 +8,7 @@ brew prune ;
 gem update ;
 gem update --system ;
 gem pristine bundler ;
+gem uninstall fileutils
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U ;
 pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U ;
 asdf update ;
