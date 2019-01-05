88fd2fd8a1ac90366b92ec1d7cfdf02a94dddaae
Devin Morgenstern <devin.morgenstern@protonmail.com>
2019-01-05 14:26:42 -0500

    New git alias, new pynvim setup

--- bin/pip.sh
+++ bin/pip.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-pip2 install neovim ;
-pip3 install neovim ;
+pip2 install --user pynvim ;
+pip3 install --user pynvim ;
 pip install yamllint ;
 pip2 install awscli --upgrade ;
 pip3 install awscli --upgrade ;
