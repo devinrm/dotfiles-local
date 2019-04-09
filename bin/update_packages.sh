53e4bef90fd345f79d5c0f7f0bcee007ca464e1d
Devin Morgenstern <devin.morgenstern@protonmail.com>
2019-04-09 09:42:59 -0400

    fixup npm command

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -8,5 +8,5 @@ gem update ;
 gem update --system ;
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U ;
 pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U ;
-npm outdated -g --depth=0
+npm outdated -g
 npm update -g
