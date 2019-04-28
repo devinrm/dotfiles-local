d2d65c038638776522ea3dfa178d4fb3eac81d7d
Devin Morgenstern <devin.morgenstern@protonmail.com>
2019-04-28 16:09:49 -0400

    go pack to pure prompt; powerline is slow and overkill

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -70,20 +70,4 @@ zle -N edit-command-line
 
 # Pure: Pretty, minimal and fast ZSH prompt
 autoload -U promptinit && promptinit
-# prompt pure
-function powerline_precmd() {
-  PS1="$($GOPATH/bin/powerline-go -newline -modules aws,docker,ssh,cwd,git -error $? -shell zsh)"
-}
-
-function install_powerline_precmd() {
-  for s in "${precmd_functions[@]}"; do
-    if [ "$s" = "powerline_precmd" ]; then
-      return
-    fi
-  done
-  precmd_functions+=(powerline_precmd)
-}
-
-if [ "$TERM" != "linux" ]; then
-  install_powerline_precmd
-fi
+prompt pure
