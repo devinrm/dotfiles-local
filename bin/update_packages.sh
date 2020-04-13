4552062cfbea219ad44b7d556563fa5368bedbb2
Devin Morgenstern <devin.morgenstern@protonmail.com>
2020-04-13 07:02:54 -0700

    cleanup update script

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -2,12 +2,9 @@
 
 set -e
 
-brew update && brew upgrade ;
-brew cleanup ;
-gem update ;
-gem update --system ;
-gem uninstall fileutils
+brew update && brew upgrade && brew cleanup ;
+gem update && gem update --system && gem uninstall fileutils ;
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U ;
 pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U ;
-npm outdated -g
-npm update -g
+npm outdated -g ;
+npm update -g ;
