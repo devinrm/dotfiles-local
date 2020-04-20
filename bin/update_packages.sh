55929843477d2f6ef15bf507d32049db6874648d
Devin Morgenstern <devin.morgenstern@protonmail.com>
2020-04-20 07:14:47 -0700

    don't use python 2 anymore

--- bin/update_packages.sh
+++ bin/update_packages.sh
@@ -4,6 +4,5 @@ set -e
 
 brew update && brew upgrade && brew cleanup ;
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U ;
-pip2 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip2 install -U ;
 npm outdated -g ;
 npm update -g ;
